4971e98b7c8a321d9497574d8e0c77b807398e2d
Avishi2511 <avishimittal2004@gmail.com>
2023-12-20 10:25:29 +0530

    added_routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './index.css';
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import About from "./Routes/About";
+import Project from "./Routes/Project";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/Contact" element={<Contact />}/>
+      <Route path="/About" element={<About />}/>
+      <Route path="/Project" element={<Project />}/>
+    </Routes>
   );
 }
 
