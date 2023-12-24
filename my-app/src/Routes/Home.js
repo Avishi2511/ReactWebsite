290e318e734abc2d332140f9bf65004430b10b8f
Avishi2511 <avishimittal2004@gmail.com>
2023-12-24 17:30:36 +0530

    footer

--- my-app/src/Routes/Home.js
+++ my-app/src/Routes/Home.js
@@ -2,12 +2,14 @@
 import React from 'react'
 import Navbar from "../Components/Navbar.js";
 import MainImg from "../Components/MainImg.js";
+import Footer from "../Components/Footer.js";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <MainImg />
+      <Footer />
     </div>
   )
 }
